76886849975e7930aac51ca7c097f5e8dd9d0d3f
ruchikh <ruchikharwar4@gmail.com>
2025-03-04 01:18:15 +0530

    Handle test case for single and multiple value

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import StringCalculator from "./StringCalculator";
+import StringCalculator from "./stringCalculator";
+
 describe("String Calculator", () => {
     test("renders input and button", () => {
         render(<StringCalculator />);
@@ -16,4 +17,27 @@ describe("String Calculator", () => {
         fireEvent.click(button);
         expect(screen.getByText("Result: 0")).toBeInTheDocument();
     });
+
+    test("returns sum for a single number", () => {
+        render(<StringCalculator />);
+        const input = screen.getByPlaceholderText("Enter numbers...");
+        const button = screen.getByText("Calculate");
+
+        fireEvent.change(input, { target: { value: "1" } });
+        fireEvent.click(button);
+
+        expect(screen.getByText("Result: 1")).toBeInTheDocument();
+    });
+
+    test("returns sum for multiple numbers", () => {
+        render(<StringCalculator />);
+        const input = screen.getByPlaceholderText("Enter numbers...");
+        const button = screen.getByText("Calculate");
+
+        fireEvent.change(input, { target: { value: "1,2,3" } });
+        fireEvent.click(button);
+
+        expect(screen.getByText("Result: 6")).toBeInTheDocument();
+    });
+
 })
